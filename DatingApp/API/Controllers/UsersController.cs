8a176058736389a45f22d065ecea9abce5571fe9
Luís Silvério <oppositedeep@gmail.com>
2023-04-18 11:37:07 +0100

    Section 1 development

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Defines the API routes for User related methods
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")] // /api/Users
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// The database context
+        /// </summary>
+        private readonly DataContext _context;
+
+        /// <summary>
+        /// /// Initializes a new instance of the <see cref="UsersController{T}"/> class.
+        /// </summary>
+        /// <param name="context"></param>
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get all the users
+        /// </summary>
+        /// <returns>Returns all the users</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets the specified user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns the user that matches the provided id</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <value></value>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <value></value>
+        public string UserName { get; set; }
+    }
+}
