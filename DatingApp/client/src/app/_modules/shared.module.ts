2e058ba2c5b1635470e0fe2e336ad639bfdf0bff
Luís Silvério <oppositedeep@gmail.com>
2023-05-04 11:03:52 +0100

    Section 10 end

--- DatingApp/client/src/app/_modules/shared.module.ts
+++ DatingApp/client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [],
@@ -12,6 +13,7 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     NgxGalleryModule,
+    NgxSpinnerModule.forRoot({type: 'line-scale-party'}),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     })
@@ -20,7 +22,8 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
     BsDropdownModule,
     NgxGalleryModule,
     TabsModule,
-    ToastrModule
+    ToastrModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
